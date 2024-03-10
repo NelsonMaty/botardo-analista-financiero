e6b4eb558a16bda4d38398a2c19d16a6969701fc
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-03-10 12:15:55 -0300

    Refactor youtube transcript extractor

---
+++ src/data_extractors/FromYoutube.py
@@ -0,0 +1,24 @@
+from pytube import YouTube
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def __get_video_id(url):
+    try:
+        return YouTube(url).video_id
+    except Exception as e:
+        return None
+
+def __convert_json_to_rst(transcript):
+    result = ""
+    for caption in transcript:
+        result += f"{caption['text']}\n\n"
+    return result
+
+def __get_transcript_from_id(video_id):
+    result = YouTubeTranscriptApi.get_transcript(video_id, languages=['es', 'en']) 
+    result = __convert_json_to_rst(result)
+    return result
+
+def get_transcript_for_video(url):
+    video_id = __get_video_id(url)
+    result = __get_transcript_from_id(video_id)
+    return result
--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
-from youtube_transcript_api import YouTubeTranscriptApi
 from bots.FinancialAdvisorBot import FinancialAdvisorBot
-
-def convert_json_to_rst(transcript):
-    result = ""
-    for caption in transcript:
-        result += f"{caption['text']}\n\n" # Each caption is separated by two newlines.
-    return result
-
-def get_transcript_for_video():
-    video_id = "hr2UmaUN5Cg"
-    result = YouTubeTranscriptApi.get_transcript(video_id, languages=['es', 'en']) 
-    result = convert_json_to_rst(result)
-    return result
+from data_extractors.FromYoutube import get_transcript_for_video 
+import sys
 
 def main():
-    transcript = get_transcript_for_video()
+    video_url = sys.argv[1]
+    transcript = get_transcript_for_video(video_url)
     bot = FinancialAdvisorBot(transcript)
     result = bot.generate_optimist_recommendation()
     result += "\n"
