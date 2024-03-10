82e463b9c3f098da07d1a907a124086e6ccfa714
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-03-10 13:44:32 -0300

    Add line between cli responses

--- src/main.py
+++ src/main.py
@@ -7,9 +7,9 @@ def main():
     transcript = get_transcript_for_video(video_url)
     bot = FinancialAdvisorBot(transcript)
     result = bot.generate_optimist_recommendation()
-    result += "\n"
-    result += bot.generate_pesimist_recommendation()
+    print(result += "\n")
+    result = bot.generate_pesimist_recommendation()
     print(result)
-    
+
 if __name__ == "__main__":
     main()
