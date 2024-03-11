c0b68e0006d1641929a73768e301cd90e914fa72
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-03-11 09:49:23 -0300

    Refine prompt

--- src/bots/FinancialAdvisorBot.py
+++ src/bots/FinancialAdvisorBot.py
@@ -8,31 +8,29 @@ class FinancialAdvisorBot(OpenAIBot):
     def generate_analysis(self):
         prompt = '''
         Hablas español.
-        Eres un buen asesor financiero.
-        Eres bueno identificando oportunidades de inversion y riesgos de inversion.
-        Sabes expresarte usando jerga tecnica.
-        Basandote en el siguiente CONTENIDO, tu tarea es extraer informacion.
+        Basandote en el siguiente CONTENIDO, tu tarea es extraer informacion precisa acerca de oportunidades y riesgos de inversion.
         Primero, describe el contexto economico del mercado actual, junto a expectativas futuras.
-        Luego, para todos los instrumentos de inversion mencionados en el CONTENIDO, elabora una respuesta donde se listen las razones para invertir y para no invertir en el activo.
+        Luego, enumera todos los nombres de activos de inversion mencionados en el CONTENIDO.
+        Siguiente, para cada uno de esos nombres de activos de inversion elabora hasta dos parrafos. 
+        El primer parrafo lista las razones para invertir en el instrumento. 
+        El segundo parrafo lista las razones para no invertir en el instrumento.
         Asegurate de separarar los activos con el delimitador: END_OF_MESSAGE
-        Nótese que puedes agregar o quitar subpuntos consideres necesario para presentar la información de manera clara, sin redundar en subpuntos.
-        EVITA INCLUIR UN RESUMEN AL FINAL DEL TEXTO.
+        Nótese que puedes agregar o quitar items en los listados como necesites para presentar la información completa.
+        Evita incluir un resumen al final del texto.
         El patron de respuesta debe ser el siguiente:
         🌐 Contexto economico
 
-        {contexto economico}
+        {contexto economico en terminos tecnicos. Expectativas mencionadas para el futuro.}
 
         END_OF_MESSAGE
 
         💰{NOMBRE DEL ACTIVO ESPECIFICO EN MAYUSCULAS (si hay ejemplos, incluirlos aqui entre prarentensis)}
 
         🟢 Por que es una buena inversion? 
-         - {subpunto explicando en menos de 10 palabras porque seria una buena decision invertir en este momento en dicho activo basandonse en el CONTENIDO}  
-         - {subpunto explicando en menos de 10 palabras porque seria una buena decision invertir en este momento en dicho activo basandonse en el CONTENIDO}  
+         - {item de listado explicando en menos de 10 palabras porque seria una buena decision invertir en este momento en dicho activo basandonse en el CONTENIDO}
          ... 
         ⚠️ Por que no es una buena inversion en este momento? 
-         - {bullet explicando en menos de 10 porque seria una mala decision invertir en este momento en dicho activo basandonse en el CONTENIDO}  
-         - {bullet explicando en menos de 10 porque seria una mala decision invertir en este momento en dicho activo basandonse en el CONTENIDO}  
+         - {item de listado explicando en menos de 10 palabras porque seria una mala decision invertir en este momento en dicho activo basandonse en el CONTENIDO}
          ...
          END_OF_MESSAGE
         '''
