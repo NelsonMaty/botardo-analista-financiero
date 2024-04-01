4e1cad0e84fe6cc5bdc4af18b19df08e66b05a44
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-04-01 17:45:27 -0300

    Adapt to render deployment template

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+import requests
+import os
+
+app = Flask(__name__)
+
+TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
+TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/"
+
+@app.route(f'/{TELEGRAM_API_KEY}', methods=['POST'])
+def respond():
+    update = request.json
+    chat_id = update['message']['chat']['id']
+    text = update['message']['text']
+    send_message(chat_id, text)
+    return '', 200
+
+
+def send_message(chat_id, text):
+    data = {
+        'chat_id': chat_id,
+        'text': text
+    }
+    response = requests.post(TELEGRAM_API_URL + 'sendMessage', data=data)
+    return response.json()
