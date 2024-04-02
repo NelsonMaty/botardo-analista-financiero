7eea86d26aef6223fd5a4a699135c0cf21f9260d
Nelson M. Rios <nelsonmatias9@hotmail.com>
2024-04-01 21:48:55 -0300

    Add forward message capability

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from src.data_extractors.FromYoutube import get_transcript_for_video 
 from src.bots.FinancialAdvisorBot import FinancialAdvisorBot
-from dotenv import load_dotenv
 from flask import Flask, request
 import os
 import requests
 
 app = Flask(__name__)
 
+MY_TELEGRAM_CHAT_ID = os.getenv("MY_TELEGRAM_CHAT_ID")
 TELEGRAM_API_KEY = os.getenv("TELEGRAM_API_KEY")
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_API_KEY}/"
 
@@ -17,11 +17,13 @@ def respond():
     text = message['text']
 
     if (text == '/start'):
-        reply = 'Hola ' + message["from"]["first_name"] + '! 👋'
+        name = message["from"]["first_name"]
+        reply = 'Hola ' + name + '! 👋'
         reply_message(chat_id, reply)
-        print('este es el chat iddddddddd ' + str(chat_id))
+        forward_message(name + ' comenzo a usar finbotardo')
     else:
         reply_message(chat_id, 'Analizando... 🤔')
+        forward_message('Me pidieron analizar: ' + text))
         transcript = get_transcript_for_video(text)
         if (transcript == '' or transcript == None):
             reply_message(chat_id, 'No se encontró información sobre este video. 😕')
@@ -29,11 +31,16 @@ def respond():
         # messages = bot.generate_analysis()
         # for message in messages:
             # reply_message(chat_id, message)
-
     return '', 200
 
 def reply_message(chat_id, message):
     requests.post(TELEGRAM_API_URL + 'sendMessage', {
         'chat_id': chat_id,
+        'text': message 
+    })
+
+def forward_message(message):
+    requests.post(TELEGRAM_API_URL + 'sendMessage', {
+        'chat_id': MY_TELEGRAM_CHAT_ID,
         'text': message 
     })
